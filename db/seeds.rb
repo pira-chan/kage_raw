b7e9ef05eaab89e73c8e38dfcb5228e844c29e1c
EC2 Default User <ec2-user@ip-172-31-24-10.ap-southeast-1.compute.internal>
2019-11-03 12:29:31 +0000

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+items_csv = CSV.readlines("db/items.csv")
+items_csv.each do |row|
+  Item.create(title: row[1], cr_item: row[2], item: row[5], created_at: row[6], updated_at: row[7], dl_id: row[8], sugest: row[9], tag2: row[10], tag3: row[11], tag4: row[12], tag5: row[13], tag6: row[14], tag7: row[15], tag8: row[16], tag9: row[17])
+end
