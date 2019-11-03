f93f6b208ab1641434a729b81609d3fb32ead166
EC2 Default User <ec2-user@ip-172-31-24-10.ap-southeast-1.compute.internal>
2019-11-03 13:00:21 +0000

    add csv4

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ require "csv"
 
 items_csv = CSV.readlines("db/items.csv")
 items_csv.each do |row|
-  Item.create(id: row[0], title: row[1], cr_item: row[2], item: row[3], created_at: row[4], updated_at: row[5], dl_id: row[6], sugest: row[7], tag2: row[8], tag3: row[9], tag4: row[10], tag5: row[11], tag6: row[12], tag7: row[13], tag8: row[14], tag9: row[15], tag10: row[16])
+  Item.create(title: row[1], tag: [2], cr_item: row[3], item: row[4], created_at: row[5], updated_at: row[6], dl_id: row[7], sugest: row[8], tag2: row[9], tag3: row[10], tag4: row[11], tag5: row[12], tag6: row[13], tag7: row[14], tag8: row[15], tag9: row[16], tag10: row[17]) 
  end
