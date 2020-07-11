81f22e1bee8f410d6e5df518ce787e65d410ee11
EC2 Default User <ec2-user@ip-172-31-24-10.ap-southeast-1.compute.internal>
2020-07-11 10:12:58 +0000

    devise メール認証　host指定２

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.app_domain= 'https://www.kage-raw.com'
+  config.app_domain = 'https://www.kage-raw.com'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -96,7 +96,7 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-　config.action_mailer.default_url_options = { host: config.app_domain }
+　config.action_mailer.default_url_options = { host: 'https://www.kage-raw.com' }
   config.action_mailer.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
