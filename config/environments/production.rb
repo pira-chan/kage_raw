6eb393121ecf3cbe0efed16e244dc307ab38ee86
EC2 Default User <ec2-user@ip-172-31-24-10.ap-southeast-1.compute.internal>
2020-07-12 06:12:31 +0000

    devise メール認証　host指定5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-　config.action_mailer.default_url_options = { host: 'kage-raw.com' }
+　config.action_mailer.default_url_options = { host: 'www.kage-raw.com' }
   config.action_mailer.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
